5585d5a469f25035623494a4636436a4859a365a
Max Katzmann <m.katzmann@me.com>
2017-08-24 14:24:20 +0200

    "Added script to automatically update versions"

--- Sources/graphlibS/Helper/SLogging.swift
+++ Sources/graphlibS/Helper/SLogging.swift
@@ -41,14 +41,28 @@ public class SLogging {
         return Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000_000
     }
     
+    
+    /// Uses GNUplot to plot the passed values to a png file int the specified directory.
+    ///
+    /// - Note: Every x-value has to have a corresponding y-value, meaning the lengths of the x and y arrays have to be equal.
+    ///
+    /// - Parameters:
+    ///   - x: An array containing the x-values of the data to be plotted.
+    ///   - y: An array containing the corresponding y-values of the data to be plotted.
+    ///   - xmin: Where the x-axis should start.
+    ///   - xmax: Where the x-axis should end.
+    ///   - ymin: Where the y-axis should start.
+    ///   - ymax: Where the y-axis should end.
+    ///   - directoryPath: The path to the directory where the file is supposed to be saved.
+    ///   - fileName: The name of the file (including the ".png" suffix) where the plot should be saved.
     public static func plotValues(x: [Double],
-                           y: [Double],
-                           xmin: Double = 0.0,
-                           xmax: Double = 1.0,
-                           ymin: Double = 0.0,
-                           ymax: Double = 1.0,
-                           toDirectory directoryPath: String,
-                           withFileName fileName: String) {
+                                  y: [Double],
+                                  xmin: Double,
+                                  xmax: Double,
+                                  ymin: Double,
+                                  ymax: Double,
+                                  toDirectory directoryPath: String,
+                                  withFileName fileName: String) {
         guard x.count == y.count else {
             SLogging.error(message: "Could not plot the passed values as the number of x values and y values did not match.")
             return
---
+++ pushUpdate.swift
@@ -0,0 +1,54 @@
+#!/usr/bin/env xcrun swift
+
+import Foundation
+
+func shell(arguments: [String]) -> String
+{
+    let task = Process()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = arguments
+
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output: String = String(data: data, encoding: String.Encoding.utf8)!
+
+    return output
+}
+
+if CommandLine.arguments.count > 1 {
+    let currentVersionString = shell(arguments: ["git", "describe", "--tags"]).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    print("Current Version is: \(currentVersionString)")
+
+    var versionComponents = currentVersionString.components(separatedBy: ".")
+
+    if let currentBuildNumberString = versionComponents.last,
+       let currentBuildNumber = Int(currentBuildNumberString) {
+
+        let newBuildNumber = currentBuildNumber + 1
+        versionComponents[versionComponents.count - 1] = String(newBuildNumber)
+        let newVersionString = versionComponents.joined(separator: ".")
+        print("New version will be: \(newVersionString)")
+
+        let commitMessage = CommandLine.arguments[1]
+        print("Commiting with message: \(commitMessage)")
+
+        let gitAddResult = shell(arguments: ["git", "add", "."])
+        print("Added: \(gitAddResult)")
+
+        let gitCommitResult = shell(arguments: ["git", "commit", "-m", "\"\(commitMessage)\""])
+        print("Commited: \(gitCommitResult)")
+
+        let gitTagResult = shell(arguments: ["git", "tag", "\(newVersionString)"])
+        print("Tagged: \(gitTagResult)")
+
+        let gitPushResult = shell(arguments: ["git", "push", "origin", "master", "--tags"])
+        print("Pushed: \(gitPushResult)")
+
+        print("Done.")
+    }
+} else {
+    print("Usage: \(CommandLine.arguments[0]) \"commit message\"")
+}
