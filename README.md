fa4ddc25a3d96c979046dd9b1557b69ade6d2159
Max Katzmann <m.katzmann@me.com>
2017-09-15 16:16:35 +0200

    Improved performance of clustering coefficient distribution algorithm.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # graphlibS
-[![GitHub tag](https://img.shields.io/badge/Version-0.0.15-brightgreen.svg)](https://github.com/maxkatzmann/graphlibS/releases/tag/0.0.15)
+[![GitHub tag](https://img.shields.io/badge/Version-0.0.16-brightgreen.svg)](https://github.com/maxkatzmann/graphlibS/releases/tag/0.0.16)
 
 This will (hopefully) become a high performance graph library written in Swift.
 Totally work in progress at the moment. I don't follow a certain schedule
@@ -53,6 +53,7 @@ here soon.
 ---
 ## License
 GPL-3.0
+
 
 
 
--- Sources/graphlibS/SAlgorithms.swift
+++ Sources/graphlibS/SAlgorithms.swift
@@ -91,12 +91,19 @@ public class SAlgorithms {
     /// and returns them in an array. The local clustering coefficient of vertex
     /// i is stored at position i in the array.
     ///
-    /// - Complexity: O(numberOfNodes * max_degree^3)
+    /// - Complexity: O(numberOfNodes * max_degree^2)
     /// - Parameter graph: The graph whose local clustering coefficiens are to be determined
     /// - Returns: An array of Double values representing the local clustering coefficients of the vertices in the graph.
     public static func localClusteringCoefficientDistribution(of graph: SGraph) -> [Double] {
         var localClusteringCoefficients = [Double](repeating: 0.0,
                                                    count: graph.numberOfVertices)
+        
+        /**
+         *  Create an array that will help us with small precomputations,
+         *  allowing us to save some time with "adjacency" queries.
+         */
+        var isNeighbor = [Bool](repeating: false,
+                                count: graph.numberOfVertices)
         
         /**
          *  Iterate all vertices in the graph in order to determine their local clustering coefficients.
@@ -121,49 +128,40 @@ public class SAlgorithms {
                 var actualEdgesAmongNeighbors = 0
                 
                 /**
+                 *  We now mark all the neighbors of v in our "isNeighbor" array
+                 *  Afterwards when iterating the neighbors of our neighbors
+                 *  we don't do adjacency checks, but rather check whether
+                 *  they are marked in this array.
+                 */
+                for u in graph.edges[v] {
+                    isNeighbor[u] = true
+                }
+                
+                /**
                  *  In the following we count all the edges that are actually in
                  *  the neighborhood of the vertex. This takes deg(v)^2 to iterate
                  *  all possible neighbor pairs. Additionally a factor of max(deg(N(v)))
                  *  is added by the adjacency check, leading to O(max_degree^3)
                  */
-                
-                /**
-                 *  If the graph is directed, we count all possible edges, which
-                 *  means between two vertices may be two edges.
-                 */
-                if graph.directed {
-                    for u1 in graph.edges[v] {
-                        for u2 in graph.edges[v] {
-                            if graph.adjacent(u: u1, v: u2) {
-                                actualEdgesAmongNeighbors += 1
-                            }
-                        }
-                    }
-                } else {
-                    /**
-                     *  If the graph is not directed, there is only one edge between
-                     *  two vertices, which makes counting a little easier.
-                     */
-                    for index1 in 0..<numberOfNeighbors - 1 {
-                        for index2 in (index1 + 1)..<numberOfNeighbors {
-                            if graph.adjacent(u: graph.edges[v][index1], v: graph.edges[v][index2]) {
-                                actualEdgesAmongNeighbors += 1
-                            }
+                for u1 in graph.edges[v] {                  // u1 is a neighbor of v
+                    for u2 in graph.edges[u1] {             // u2 is a neighbor of u1
+                        if isNeighbor[u2] {                 // if u2 is also a neighbor of v...
+                            actualEdgesAmongNeighbors += 1  // ... then we have one more edge amongst our neighbors.
                         }
                     }
                 }
                 
-                var localClusteringCoefficient = Double(actualEdgesAmongNeighbors) / Double(possibleEdgesAmongNeighbors)
-                
-                if !graph.directed {
-                    localClusteringCoefficient *= 2.0
-                }
-                
-                if localClusteringCoefficient != localClusteringCoefficient {
-                    SLogging.error(message: "NaN!")
-                }
+                let localClusteringCoefficient = Double(actualEdgesAmongNeighbors) / Double(possibleEdgesAmongNeighbors)
                 
                 localClusteringCoefficients[v] = localClusteringCoefficient
+                
+                /**
+                 *  Finally, we reset our "isNeighbor" array, such that when
+                 *  processing the next vertex we don't old neighbors marked.
+                 */
+                for u in graph.edges[v] {
+                    isNeighbor[u] = false
+                }
             }
         }
         
